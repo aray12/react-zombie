6c34fd6b576b606362e432b30f2f55048cf49e04
Alex Ray <alex.ray12@gmail.com>
2015-10-20 14:50:18 -0700

    Added foundation for testing

---
+++ tests/test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alexanderray on 10/20/15.
+ */
+
+'use strict';
+
+var assert = require('assert');
+
+describe('App', function() {
+	it('should just work', function(done) {
+		assert.equal(1, 1);
+
+		done();
+	});
+});
